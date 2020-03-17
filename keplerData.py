9d84aff6bc112955c8277baf97da6d09d431eaf3
Alexander Schacht <a.schacht24@gmail.com>
2020-03-17 10:13:38 +0100

    actually plots data now

--- keplerData.py
+++ keplerData.py
@@ -1,28 +1,26 @@
 import lightkurve as lk
+import matplotlib.pyplot as plt
 
 def getKeplerData():
-    pixels = lk.search_targetpixelfile("Kepler-10").download()
-    pixels.plot()
+    #pixels = lk.search_targetpixelfile("Kepler-10").download_all()
+    # pixels.plot()
+    # plt.show()
+    lightcurve = lk.search_lightcurvefile("Kepler-10").download_all()
+    lightcurve.PDCSAP_FLUX.plot()
+    plt.show()
+    for elem in lightcurve:
+        elem.plot()
+        plt.show()
 
-    lightcurve = pixels.to_lightcurve()
-    lightcurve.plot()
+        exoplanet = elem.flatten().fold(period=0.838)
+        exoplanet.plot()
+        plt.show()
 
-    exoplanet = lightcurve.flatten().fold(period=0.838)
-    exoplanet.plot()
-    print("type of the pixels var is", type(pixels))
-    print(pixels)
+    # exoplanet = lightcurve.flatten().fold(period=0.838)
+    # exoplanet.plot()
+    # plt.show()
+    # print("type of the pixels var is", type(pixels))
+    # print(pixels)
 
 if __name__ == "__main__":
-    '''
-    pixels = lk.search_targetpixelfile("Kepler-10").download_all()
-    pixels.plot()
-
-    lightcurve = pixels.to_lightcurve()
-    lightcurve.plot()
-
-    exoplanet = lightcurve.flatten().fold(period=0.838)
-    exoplanet.plot()
-    print("type of the pixels var is", type(pixels))
-    print(pixels)
-    '''
     getKeplerData()
