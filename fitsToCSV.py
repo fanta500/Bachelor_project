36171795f189ea14f87646e783697577ebb673c0
Alexander Schacht <a.schacht24@gmail.com>
2020-03-19 17:27:45 +0100

    Converting every file in directory from .fits to .csv. Change directory name as appropriate for your machine

--- fitsToCSV.py
+++ fitsToCSV.py
@@ -3,17 +3,29 @@
 # written by Erin for Jim! 
 
 import numpy
+import os
 from astropy.io import fits
 
+# directory to use
+directory = 'G:/KEPLER_DATA/Q0/'
+directorySize = len(os.listdir(directory))
+filesProcessed = 1
+progress = 0.0
 # ask for an input file 
-filename = raw_input('Enter a FITS file to CSV-ize: ')
+print("Processing...", "0 %")
+for filename in os.listdir(directory):
+    if filename.endswith(".fits"):
+        # Open the given fits file
+        hdulist = fits.open(directory+filename)
+        scidata = hdulist[-1].data
 
-# ask for an output file name
-output = raw_input('What would you like to call your new file?: ')
+        # ask for an output file name
+        outputFileName = filename.split('.')[0]
+        output = directory + outputFileName + ".csv"
 
-# Open the given fits file
-hdulist = fits.open(filename)
-scidata = hdulist[0].data
-
-# save your new file as a csv
-numpy.savetxt(output, scidata, fmt='%d', delimiter=',')
+        # save your new file as a csv
+        numpy.savetxt(output, scidata, fmt='%d', delimiter=',')
+    if round((filesProcessed/directorySize)*100, 1) > progress:
+        progress += 1
+        print("Processing...", round((filesProcessed/directorySize)*100, 1), " %")
+    filesProcessed += 1
