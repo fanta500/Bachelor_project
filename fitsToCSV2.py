851daff7c7cdc904b2af91798de228d9e7f0a904
guldtop <jona_berg@hotmail.com>
2020-03-24 18:53:52 +0100

    Updated fitsToCSV2 file, takes all .fits files and outputs a single .csv file

--- fitsToCSV2.py
+++ fitsToCSV2.py
@@ -4,8 +4,12 @@ import csv
 from astropy.io import fits
 
 
+not_yet_called = True       # flag argument for column name list creation
+csv_columns = []
+glbl_dict = []              # collection of all the datapoints (all the .fits files)
+
 # directory to use
-directory = 'G:/KEPLER_DATA/Q0/'
+directory = '/Users/jonathanscomputer/Desktop/Uni/Bachelor/Code/astropy/'
 directorySize = len(os.listdir(directory))
 filesProcessed = 1
 progress = 0.0
@@ -13,24 +17,34 @@ progress = 0.0
 print("Processing...", "0 %")
 for filename in os.listdir(directory):
     if filename.endswith(".fits"):
-        # Open the given fits file
+
         hdulist = fits.open(directory+filename)
 
         my_dict = {}
-
+        
         for page in range(len(hdulist)-1):
             for item in hdulist[page].header:
+                if(not_yet_called):             # need to get column names only once
+                    csv_columns.append(item)    # add item name to list of names
                 my_dict[item] = hdulist[page].header[item]
+        
+        not_yet_called = False 
 
         scidata = my_dict
-
         outputFileName = filename.split('.')[0]
 
-        with open(directory+outputFileName+'.csv', 'w') as f:
-            for key in my_dict.keys():
-                f.write("%s,%s\n"%(key,my_dict[key]))
-                
+        glbl_dict.append(my_dict)               # gather all datapoints in a single list
+
     if round((filesProcessed/directorySize)*100, 1) > progress:
         progress += 1
         print("Processing...", round((filesProcessed/directorySize)*100, 1), " %")
     filesProcessed += 1
+
+try:
+    with open(directory+'data_as_csv'+'.csv', 'w') as csvfile:      # write all the datapoints into a single .csv file
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in glbl_dict:
+            writer.writerow(data)
+except IOError:
+    print("I/O error")
